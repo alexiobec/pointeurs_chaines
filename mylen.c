2b2aa02be4ce068dabcb48a04a1150d35a2a5a2d
Alexio Bec <alexio.bec@gmail.com>
2021-06-03 11:03:54 +0200

    version debugguée Please enter the commit message for your changes. Lines starting

--- mylen.c
+++ mylen.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include "mylen.h"
 
-int main( ) {
-    printf( "%d"\n , mylen( "abcde" ) )
+void main( ) {
+    printf( "%d\n " , mylen( "abcde" ) );
 }
 
 int mylen( char * s ) {
     int compteur = 0;
     while ( *s++ ) {
-        compteur++
+        compteur++;
     }
-    return( compteur )
+    return( compteur );
 }
