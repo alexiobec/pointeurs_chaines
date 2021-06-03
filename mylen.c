969b12523fcae43d79f3943d4a1ff5c10d7ada2d
Alexio Bec <alexio.bec@gmail.com>
2021-06-03 11:47:04 +0200

    ca marche pas

--- mylen.c
+++ mylen.c
@@ -3,6 +3,7 @@
 
 void main( ) {
     printf( "%d\n " , mylen( "abcde" ) );
+    printf( "%d\n " , mycopy( "abcde" ) );
 }
 
 int mylen( char * s ) {
@@ -11,4 +12,16 @@ int mylen( char * s ) {
         compteur++;
     }
     return( compteur );
+}
+
+char * mycopy( char * s ) {
+    int taille = mylen( s );
+    char * newS = malloc( ( taille + 1 ) * sizeof( char ) );
+    char * tmp = newS
+        while ( *s++ ) {
+            *tmp = *s;
+        }
+    *( tmp + 1 ) = '/0';
+    printf( "s\n" , newS );
+    return( newS );
 }
