568cec4486c972557e2115e785092bbe932187ea
Alexio Bec <alexio.bec@gmail.com>
2021-06-07 23:34:34 +0200

    j'ai enfin réussi qqch qui marche, la suite demain

--- mylen.c
+++ mylen.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include "mylen.h"
+#include <stdlib.h>
 
 void main( ) {
-    printf( "%d\n " , mylen( "abcde" ) );
-    printf( "%d\n " , mycopy( "abcde" ) );
+    char * alphabet = "abcde";
+    //printf( "%d\n " , mylen( mycopy( alphabet ) ) );
+    //printf( "original : %p \ncopie : %p \n " , &alphabet , mycopy( alphabet ) );
+    //printf( "ma copie en str donne :" );
+    //myprint( mycopy( alphabet ) );
+    //printf( "compte de e dans aebecedeee : %d\n" , comptage( "aebecedeee" , 'e' ) );
 }
 
 int mylen( char * s ) {
@@ -16,12 +21,41 @@ int mylen( char * s ) {
 
 char * mycopy( char * s ) {
     int taille = mylen( s );
+    int i = 0;
+    //printf( "taille = %d\n" , taille );
     char * newS = malloc( ( taille + 1 ) * sizeof( char ) );
-    char * tmp = newS
-        while ( *s++ ) {
-            *tmp = *s;
-        }
-    *( tmp + 1 ) = '/0';
-    printf( "s\n" , newS );
+    //char * memoire = newS;
+    while ( *( s + i ) ) {
+        *( newS + i ) = *( s + i );
+        //printf( "*newS = %c , *memoire = %c\n" , *newS , *memoire );
+        i++;
+    }
+    *( newS + i + 1 ) = *"\0";
     return( newS );
+}
+
+void myprint( char * s ) {
+    while ( *s ) {
+        printf( "%c" , *s );
+        s++;
+    }
+    printf( "\n" );
+}
+
+
+int comptage( char * mot , char lettre ) {
+    int compte = 0;
+    while ( *mot ) {
+        if ( *mot == lettre ) {
+            compte++;
+        }
+        *mot++;
+    }
+    return compte;
+}
+
+char * remplacer( char * mot , char lettre ) {
+    char * copie = mycopy( char * mot );
+    *copie = lettre;
+    return( copie );
 }
--- mylen.h
+++ mylen.h
@@ -1,1 +1,4 @@
 int mylen( char * s );
+char * mycopy( char * s );
+void myprint( char * s );
+int comptage( char * mot , char lettre );
